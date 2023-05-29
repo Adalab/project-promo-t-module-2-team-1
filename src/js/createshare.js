5c1dd9ed1939b6593cf6b8939b29b987de229ea2
CCarolinaFranco <cirilina@hotmail.com>
2023-05-29 12:51:08 +0100

    innerhtml cambios

--- src/js/createshare.js
+++ src/js/createshare.js
@@ -25,3 +25,8 @@ function clickedcolour() {
 }
 
 btn.addEventListener('click', clickedcolour);
+
+
+/*<p class="tarjeta">La tarjeta ha sido creada:</p>
+                <p class="link">aqui aparece un link</p>
+                <button class="bnttwitter"><i class="fa-brands fa-twitter fa-xl" style="color: #ffffff;"></i> Compartir en twitter</button>*/
